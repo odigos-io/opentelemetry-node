5d46eec5b03f9866f1fae806058e5d4f1c32eff5
Amir Blum <amirgiraffe@gmail.com>
2026-02-10 13:50:38 +0200

    feat: set agent logger level with OTEL_LOG_LEVEL

---
+++ src/diag.ts
@@ -0,0 +1,28 @@
+import { DiagConsoleLogger, DiagLogLevel, diag } from "@opentelemetry/api";
+
+// The support string -> DiagLogLevel mappings
+const logLevelMap: { [key: string]: DiagLogLevel } = {
+    ALL: DiagLogLevel.ALL,
+    VERBOSE: DiagLogLevel.VERBOSE,
+    DEBUG: DiagLogLevel.DEBUG,
+    INFO: DiagLogLevel.INFO,
+    WARN: DiagLogLevel.WARN,
+    ERROR: DiagLogLevel.ERROR,
+    NONE: DiagLogLevel.NONE,
+};
+
+export const getOtelLogLevel = (): DiagLogLevel | undefined => {
+    const otelLogLevel = process.env['OTEL_LOG_LEVEL'];
+    if (!otelLogLevel) {
+        return undefined;
+    }
+    return logLevelMap[otelLogLevel.toUpperCase()];
+}
+
+export const setOtelDiagLogger = () => {
+    const logLevel = getOtelLogLevel();
+    if (!logLevel) {
+        return;
+    }
+    diag.setLogger(new DiagConsoleLogger(), logLevel);
+}
